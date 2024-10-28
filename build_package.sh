c2ce9c850bbd1775f0e37a84efdcaa66fecbcf2d
Bardia Moshiri <bardia@furilabs.com>
2024-10-27 21:46:40 -0400

    build_package: install debian-staging too on staging builds
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- build_package.sh
+++ build_package.sh
@@ -247,7 +247,7 @@ fi
 # Enable staging repository for staging builds
 if [ "${BUILD_TYPE}" == "staging" ] && [ "${IS_CONTAINER}" != "true" ]; then
 	info "Enabling staging repository"
-	apt-get --yes install furios-apt-config-staging
+	apt-get --yes install furios-apt-config-staging furios-apt-config-debian-staging
 fi
 
 # Refresh APT database
